5f180a90880a0d28eb70431cc7f571cc39272e13
Daniel Groos <daniegr@stud.ntnu.no>
2016-09-28 13:51:28 +0200

    Create Identify Species View

---
+++ NbicDragonflies/NbicDragonflies/NbicDragonflies/Models/ResultItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+
+namespace NbicDragonflies.Models
+{
+
+    class ResultItem
+    {
+        public string ImageSource { get; set; }
+        public string Text { get; set; }
+        public Type TargetType { get; set; }
+    }
+
+}
---
+++ NbicDragonflies/NbicDragonflies/NbicDragonflies/Views/Identify.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NbicDragonflies.Models;
+using Xamarin.Forms;
+
+namespace NbicDragonflies.Views {
+    public partial class Identify : ContentPage {
+
+        public ListView ListView { get { return ResultsList; } }
+
+        public Identify()
+        {
+            InitializeComponent();
+
+            var resultsItems = new List<ResultItem>();
+
+            resultsItems.Add(new ResultItem
+            {
+                ImageSource = "dragonfly1.jpg",
+                Text = "Dragonfly 1",
+                TargetType = typeof(Home)
+            });
+
+            resultsItems.Add(new ResultItem
+            {
+                ImageSource = "dragonfly2.jpg",
+                Text = "Dragonfly 2",
+                TargetType = typeof(Home)
+            });
+
+            ResultsList.ItemsSource = resultsItems;
+        }
+    }
+}
