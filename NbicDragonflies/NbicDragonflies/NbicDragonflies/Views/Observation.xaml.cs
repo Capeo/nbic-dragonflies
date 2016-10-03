cd67b56a592f6650deced011b3099c5bfe5bb9c1
Yara Fahmy <yfahmy610@gmail.com>
2016-10-03 04:10:17 +0200

    Added Tabs to Observation Page with list of Observations

--- NbicDragonflies/NbicDragonflies/NbicDragonflies/Views/Observation.xaml.cs
+++ NbicDragonflies/NbicDragonflies/NbicDragonflies/Views/Observation.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using NbicDragonflies.Models;
+using NbicDragonflies.Views.ListItems;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -9,44 +10,13 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 
 
-/*
- var navigationPage = new NavigationPage (new SchedulePageCS ());
-    navigationPage.Icon = "schedule.png";
-    navigationPage.Title = "Schedule";
-
-    Children.Add (new TodayPageCS ());
-    Children.Add (navigationPage);
-     */
-
 namespace NbicDragonflies.Views
 {
     public partial class Observation : TabbedPage
     {
         public Observation()
         {
-            var observationListPage 
             InitializeComponent();
-            ObservableCollection<ObservationListItem> observationsList = new ObservableCollection<ObservationListItem>();
-
-            observationsList.Add(new ObservationListItem
-            {
-                Id = 1234,
-                ObservedOn = "2012-04-10",
-                PlaceGuess = "Trondheim, Sor-Trondelag",
-                UserLogin = "thedragonslayer",
-                SpeciesGuess = "DragonFly"
-            });
-
-            observationsList.Add(new ObservationListItem
-            {
-                Id = 1235,
-                ObservedOn = "2016-04-10",
-                PlaceGuess = "Bergen, Hordland",
-                UserLogin = "theotherdragonslayer",
-                SpeciesGuess = "DamselFly"
-            });
-
-            ObservationList.ItemsSource = observationsList;
         }
     }
 }
---
+++ NbicDragonflies/NbicDragonflies/NbicDragonflies/Views/ObservationList.xaml.cs
@@ -0,0 +1,46 @@
+﻿using NbicDragonflies.Views.ListItems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace NbicDragonflies.Views
+{
+    public partial class ObservationList : ContentView
+    {
+        public ObservationList()
+        {
+            InitializeComponent();
+            var recentObservations = new List<ObservationsCell>();
+
+            recentObservations.Add(new ObservationsCell
+            {
+                Species = "Brun øyenstikker",
+                LocationTime = "Trondehim, 30.09.16",
+                User = "Odd Cappelen",
+                ImageFilename = "dragonfly2.jpg"
+            });
+
+            recentObservations.Add(new ObservationsCell
+            {
+                Species = "Blå øyenstikker",
+                LocationTime = "Trondheim, 29.09.16",
+                User = "Odd Cappelen",
+                ImageFilename = "dragonfly1.jpg"
+            });
+
+            recentObservations.Add(new ObservationsCell
+            {
+                Species = "Gul øyenstikker",
+                LocationTime = "Trondheim, 28.09.16",
+                User = "Odd Cappelen",
+                ImageFilename = ""
+            });
+
+            RecentObservationsList.ItemsSource = recentObservations;
+        }
+    }
+}
