e0e9859a8efe2b736be2fd93de22ed48fd36902a
Jonas Løchsen <jonas.lochsen@gmail.com>
2016-10-24 11:01:54 +0200

    removed frames

--- NbicDragonflies/NbicDragonflies/NbicDragonflies/Utility/Constants.cs
+++ NbicDragonflies/NbicDragonflies/NbicDragonflies/Utility/Constants.cs
@@ -13,6 +13,9 @@ namespace NbicDragonflies.Utility {
         public static readonly Color NbicBrown = Color.FromHex("4a4846");
         public static readonly Color NbicOrange = Color.FromHex("e86c19");
 
+		//Page background
+		public static readonly Color NbicBackground = Color.FromHex("e0e0e0");
+
         // Hierarchy of orders
         public static readonly List<string> TaxonRanks = new List<string> { "kingdom", "phylum", "subphylum", "class", "order", "suborder", "family", "genus", "species" };
     }
