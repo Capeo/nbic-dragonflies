001e3d3a5b42130b7d23bc1f450ccb65cebf55b4
Daniel Groos <daniegr@stud.ntnu.no>
2016-10-28 15:46:47 +0200

    Add framework for Unit Testing and one example of a Unit Test

---
+++ NbicDragonflies/NbicDragonflies/NbicDragonfliesTests/UtilityTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NbicDragonfliesTests
+{
+    [TestClass]
+    public class UtilityTests
+    {
+        [TestMethod]
+        public void CapitalFirstLetter_ValidString_FirstLetterCapitalized()
+        {
+            // Arrange
+            string originalString = "odonata";
+            string expectedModifiedString = "Odonata";
+
+            // Act
+            string actualModifiedString = NbicDragonflies.Utility.Utilities.CapitalizeFirstLetter(originalString);
+
+            // Assert
+            Assert.AreEqual(expectedModifiedString, actualModifiedString);
+        }
+    }
+}
